cf2d8f3188f976a03fa1b49d53840808f8993361
Bartosz Szczepkowski <juliuszplaygames@gmail.com>
2021-04-13 14:02:36 +0100

    Hello World!

---
+++ ConsoleMenu/MenuConstructor.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+class MenuConstructor
+{
+
+
+     static int user_selected_option = 0;
+    
+
+    public static void Display(string[] options, string menu_name, int option_selected = 0)
+    {
+
+        int index = 0;
+        Console.Clear();
+        Console.WriteLine($"==========================[ {menu_name} ]==========================");
+        foreach (string o in options)
+        { 
+            if (index == option_selected)
+            {
+                Console.WriteLine(o + "         <==");
+            }
+            else {
+                Console.WriteLine(o);
+            }
+            index++;
+
+        }
+        Console.WriteLine($"==========================[ {menu_name} ]==========================");
+
+        ConsoleKey key = Console.ReadKey().Key;
+        if (key == ConsoleKey.UpArrow)
+        {
+            if (option_selected > 0)
+            {
+                MenuConstructor.Display(options, menu_name, option_selected - 1);
+            }
+            else
+            {
+                MenuConstructor.Display(options, menu_name, options.Length - 1);
+            }
+
+        }
+        else if (key == ConsoleKey.DownArrow)
+        {
+            if (option_selected + 1 < options.Length)
+            {
+                MenuConstructor.Display(options, menu_name, option_selected + 1);
+            }
+            else
+            {
+                MenuConstructor.Display(options, menu_name, 0);
+            }
+
+        }
+        else if (key == ConsoleKey.Enter)
+        {
+            MenuConstructor.user_selected_option = option_selected + 1;
+        }
+
+
+
+    }
+    public static int GetUserSelectedOption()
+    {
+        return MenuConstructor.user_selected_option;
+    }
+
+}
---
+++ ConsoleMenu/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleMenu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] menu_options = new string[] {"1","2","3","4"};
+            MenuConstructor.Display(menu_options,"Main Menu", 0);
+
+            switch (MenuConstructor.GetUserSelectedOption())
+            {
+                case 1:
+                    Console.WriteLine("asdasdasd");
+                    break;
+                case 2:
+                    Console.WriteLine("sadasd213123");
+                    break;
+            }
+        }
+    }
+}
