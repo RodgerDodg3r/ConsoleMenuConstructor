8d5a5d1d60f0fa46d128f1da0112ae0409ec22da
Bartosz Szczepkowski <66213918+MrJuliuszByte@users.noreply.github.com>
2021-04-15 11:12:43 +0100

    1.1
    Adding a example.cs as example

---
+++ ConsoleMenu/example.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+class example
+{
+    public static void Main()
+    {
+        string[] options = new string[] { "1. New Game", "2. Load Game", "3. Exit" };
+        MenuConstructor.Display(options, "RPG GAME", 0);
+        switch (MenuConstructor.GetUserSelectedOption())
+        {
+            case 1:
+                Console.WriteLine("You have selected 1st option");
+                break;
+            case 2:
+                Console.WriteLine("You have selected 2nd option");
+                break;
+            case 3:
+                Console.WriteLine("You have selected 3rd option");
+                break;
+        }
+    }
+
+
+}
